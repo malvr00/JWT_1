d8c0d4bccfd14d4316c4353f0b535cce8ff58806
조용일 <yedn1128@naver.com>
2023-01-03 14:13:03 +0900

    스프링부트 시큐리티 20강 - jwt를 위한 security 설정 ( security 설정 + cors 설정 )

---
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.cos.jwt.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity  // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨.
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CorsFilter corsFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 하지 않겠다는 옵션
+                .and()
+                .addFilter(corsFilter)  // @CrossOrigin(인증X), 시큐리티에 필터에 등록 해서 사용 하는 필터는 인증이 필요할 때 (인증 O)
+                .formLogin().disable()
+                .httpBasic().disable()  // -- 이 위까지 JWT 기본 옵션으로 사용해야 함.
+                .authorizeRequests()
+                .antMatchers("/api/v1/user/**")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/manager/**")
+                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/admin/**")
+                .access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+
+        return http.build();
+    }
+}
