25078f6d63adebfaa5bd9d2bf51600dd5e02ddef
조용일 <yedn1128@naver.com>
2023-01-03 19:07:19 +0900

    회원가입 누락 부분 추가

--- src/main/java/com/cos/jwt/comtroller/RestApiController.java
+++ src/main/java/com/cos/jwt/comtroller/RestApiController.java
@@ -1,13 +1,20 @@
 package com.cos.jwt.comtroller;
 
+import com.cos.jwt.config.app.CustomBCryptPasswordEncoder;
+import com.cos.jwt.model.User;
+import com.cos.jwt.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class RestApiController {
+
+    private final UserRepository userRepository;
+    private final CustomBCryptPasswordEncoder customBCryptPasswordEncoder;
 
     @GetMapping("/home")
     public String home() {
@@ -18,4 +25,13 @@ public class RestApiController {
     public String token(){
         return "<h1>토큰</h1>";
     }
+
+    @PostMapping("/join")
+    public String join(@RequestBody User user){
+        user.setPassword(customBCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles("ROLE_USER");
+        userRepository.save(user);
+        return "회원가입 완료";
+    }
+
 }
---
+++ src/main/java/com/cos/jwt/config/app/CustomBCryptPasswordEncoder.java
@@ -0,0 +1,8 @@
+package com.cos.jwt.config.app;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomBCryptPasswordEncoder extends BCryptPasswordEncoder {
+}
